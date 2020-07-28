2a095d1f834d4513983401e83941b772031576ae
Bill Morrisson <billmorrissonjr@gmail.com>
2020-07-28 09:48:54 -0400

    Adding exception handling for text upon creation

---
+++ src/BBServException.h
@@ -0,0 +1,27 @@
+// BBServException.h
+
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <string>
+
+/**
+ *Exception type accepting an error text upon creation.
+ */
+class BBServException : public std::exception
+{
+    std::string errorText;
+
+    public:
+        BBServException(const std::string_view& text)
+            : std::exception()
+            , errorText(text)
+        {
+        }
+
+        virtual const char* what() const noexcept
+        {
+            return this->errorText.data();
+        }
+};
