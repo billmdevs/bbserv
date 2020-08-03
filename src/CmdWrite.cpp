90f72c2fdc90d0246f74b9f3a56492c613578edc
Bill Morrisson <billmorrissonjr@gmail.com>
2020-08-03 09:45:21 -0400

    Fix double-delete problem on file streams

--- src/CmdWrite.cpp
+++ src/CmdWrite.cpp
@@ -2,9 +2,12 @@
 
 #include "CmdWrite.h"
 #include <cstring>
+#include <iomanip>
 #include <string>
 #include <string_view>
 #include <fstream>
+#include <algorithm>
+#include "ConnectionQueue.h"
 
 size_t CmdWrite::update_message_number()
 {
@@ -12,7 +15,7 @@ size_t CmdWrite::update_message_number()
     auto &out =  std::ios_base::out;
     auto &trunc =  std::ios_base::trunc;
 
-    std::string_view noFile { Config::singleton().get_bbfile() + ".no" };
+    std::string noFile { Config::singleton().get_bbfile() + ".no" };
     std::fstream io(noFile.data());
     size_t number;
 
@@ -31,9 +34,47 @@ size_t CmdWrite::update_message_number()
 
     if (io.fail())
     {
-        error_return(this, "Failed to read/write message number (", noFile, ")");
+        error_return(this, "Failed to read/write message number (", noFile, "), state=",
+                name_statebits(io.rdstate()));
     }
+
+    debug_print(this, "Current message number: ", number);
     return number;
+}
+
+void CmdWrite::broadcast_synchronous(std::string commandId, std::string userName, size_t messageId, std::string arguments)
+{
+    BroadcastCommand broadcast;
+
+    for (auto& peer : Config::singleton().get_peers())
+    {
+        debug_print(this, "Add ", commandId, "/peer to connectionQueue: ", peer);
+        broadcast.peer = peer;
+        broadcast.command = "BROADCAST-";
+        broadcast.command += commandId;
+        broadcast.command += " " + userName;
+        broadcast.command += " " + std::to_string(messageId);
+        broadcast.command += " " + arguments;
+        this->connectionQueue->add(broadcast);
+    }
+
+    // Wait for acknowledges from all peers
+    if (!AcknowledgeQueue::TheOne(messageId)->check_success(Config::singleton().get_peers().size()))
+    {
+        error_return(this, "Did not get positive acknowledge from all peers");
+    }
+    debug_print(this, "All peers acknowledged");
+    // Delete this AcknowledgeQueue
+    AcknowledgeQueue::TheOne(messageId, true);
+}
+
+bool CmdWrite::replicate_command(std::string userName, size_t messageId)
+{
+    broadcast_synchronous("PRECOMMIT", userName, messageId, "");
+    broadcast_synchronous("COMMIT", userName, messageId, commandId + " LOCAL" +
+            (this->line + commandId.size()));
+
+    return true;
 }
 
 void CmdWrite::execute()
@@ -48,18 +89,32 @@ void CmdWrite::execute()
         return;
     }
 
-    debug_print(this, "Processing ", COMMAND_ID, " command\n");
+    debug_print(this, "Processing ", COMMAND_ID, " command");
 
     std::string message { this->line + std::strlen(COMMAND_ID) + 1 };
 
+    // Ignore some leading spaces
+    auto localWriteOnly { 3 > message.find("LOCAL ") };
+    if (localWriteOnly)
+    {
+        // Eliminate the prefix
+        auto diff { std::strlen("LOCAL ") };
+        message.replace(message.begin(), message.end() - diff, message.begin()
+                + diff, message.end());
+        message.replace(message.end() - diff, message.end(), diff, '\0');
+    }
+
     try
     {
+        // Get a new message ID and open the DB file
         auto id { update_message_number() };
         std::fstream fout (Config::singleton().get_bbfile());
 
-        // Create the file if needed
+        // Create the DB file if needed
         if (fout.fail())
         {
+            debug_print(this, "Creating file: ",
+                    Config::singleton().get_bbfile());
             fout.open(Config::singleton().get_bbfile(), in|out|trunc);
             fout.flush();
         }
@@ -67,18 +122,36 @@ void CmdWrite::execute()
         // Throw if this fails too
         if (fout.fail())
         {
-            error_return(this, "Failed to open/create file ", Config::singleton().get_bbfile());
+            error_return(this, "Failed to open/create file ",
+                    Config::singleton().get_bbfile());
         }
 
+        if (this->connectionQueue && !localWriteOnly)
+        {
+            // Have all the peers process this command as well
+            if (!replicate_command(this->user, id))
+            {
+                // TODO undo
+            }
+        }
+
+        // Finally, the local write operation
         fout.seekp(0, std::ios_base::end);
-        debug_print(this, "file pos ", fout.tellp());
-        fout << id << "/" << this->user << "/" << message;
+        debug_print(this, "File pos ", fout.tellp());
+        fout << id << "/" << this->user << "/" << message.data();
+
+        if (localWriteOnly)
+        {
+            fout << std::endl;
+        }
 
         fprintf(this->stream, "3.0 WROTE %lu\n", id);
         fflush(this->stream);
     }
     catch (const BBServException& error)
     {
+        // TODO undo
+
         fprintf(this->stream, "3.2 ERROR WRITE %s\n", error.what());
         fflush(this->stream);
     }
--- src/CmdWrite.h
+++ src/CmdWrite.h
@@ -5,6 +5,7 @@
 #include <string>
 #include "SessionResources.h"
 #include "Config.h"
+#include "ConnectionQueue.h"
 
 /**
  *Command supporting the WRITE message.
@@ -17,16 +18,18 @@ class CmdWrite
         FILE* stream {nullptr};
         const char* line {nullptr};
         std::string user;
+        ConnectionQueue* connectionQueue {nullptr};
 
     public:
         /**
          *Custom constructor accepting the command line received on the socket.
          */
-        CmdWrite(const std::string& commandId, FILE* stream, const char* line, std::string& user)
+        CmdWrite(const std::string& commandId, FILE* stream, const char* line, std::string& user, ConnectionQueue* qu)
             : commandId(commandId)
             , stream(stream)
             , line(line)
             , user(user)
+            , connectionQueue(qu)
         { }
 
     public:
@@ -37,8 +40,18 @@ class CmdWrite
 
     protected:
         /**
-         *Retrive the next free message number and update the storage imediately.
+         * Retrieve the next free message number and update the storage immediately.
          */
         size_t update_message_number();
+
+        /**
+         *Broadcast this WRITE command to known peers.
+         */
+        bool replicate_command(std::string userName, size_t messageId);
+
+        /**
+         *Broadcast one command and wait for the acknowledge.
+         */
+        void broadcast_synchronous(std::string commandId, std::string userName, size_t messageId, std::string arguments);
 };
 
