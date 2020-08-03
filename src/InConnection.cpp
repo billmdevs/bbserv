bd611d5bd23919d6b8cbb61f0afcf4face22a1cd
Bill Morrisson <billmorrissonjr@gmail.com>
2020-08-03 09:44:18 -0400

    Fix a send/recv problem on non-blocking socket

--- src/InConnection.cpp
+++ src/InConnection.cpp
@@ -1,28 +1,75 @@
 #include "InConnection.h"
 #include <cstring>
+#include <memory>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <errno.h>
+#include <pthread.h>
 #include "Config.h"
 
-InConnection::InConnection(std::shared_ptr<ConnectionQueue>& qu)
+/**
+ *Data passed on to the thread/context.
+ */
+struct ContextData
 {
-    this->connectionQueue = qu;
-}
+    ContextData(InConnection* self, in_port_t port)
+        : self(self)
+        , port(port)
+    {};
 
-InConnection::~InConnection()
+    InConnection* self {nullptr};
+    in_port_t port {0};
+};
+
+/**
+ *The thread's main entry point.
+ */
+static void* thread_main(void* p)
 {
-    if (this->acceptSocket) {
-       close(this->acceptSocket);
-       this->acceptSocket = 0;
-    }
+    auto contextData { reinterpret_cast<ContextData*>(p) };
+    auto port { contextData->port };
+    auto self { contextData->self };
+    delete contextData;
+
+    self->listen_on(port);
+
+    return nullptr;
 }
 
 void InConnection::listen_on(/*const std::string_view& ipaddress,*/ in_port_t port)
 {
     open_incoming_conn(/*ipaddress,*/ port);
     listen_for_clients();
+}
+
+void InConnection::operate(/*const std::string_view& ipaddress,*/ in_port_t port,
+        std::shared_ptr<ConnectionQueue>& qu, bool isNonblocking)
+{
+    this->connectionQueue = qu;
+    this->isNonblocking = isNonblocking;
+
+    pthread_t context;
+    pthread_attr_t contextOptions;
+    pthread_attr_init(&contextOptions);
+    pthread_attr_setdetachstate(&contextOptions, PTHREAD_CREATE_DETACHED);
+
+    auto contextData = new ContextData(this, port);
+
+    bool success { 0 == pthread_create(&context, &contextOptions,
+            thread_main, reinterpret_cast<void*>(contextData))};
+
+    if (!success)
+    {
+        error_return(this, "Failed to create context: ", strerror(errno));
+    }
+
+    pthread_attr_destroy(&contextOptions);
+    debug_print(this, "Context created and ready");
 }
 
 /**
@@ -32,73 +79,129 @@ void InConnection::open_incoming_conn(/*const std::string_view& ipaddress,*/ in_
 {
     using std::string_view;
 
-    int enable = 1;
-    struct sockaddr_in acceptAddress;
-    socklen_t addressSize = sizeof(struct sockaddr_in);
+    addrinfo hints, *result;
 
-    memset(&acceptAddress, 0, addressSize);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    auto portId { std::to_string(port) };
 
-    this->acceptSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (0 > this->acceptSocket)
+    auto status { getaddrinfo(nullptr, portId.data(), &hints, &result) };
+    if (0 != status)
     {
-        error_return(this, "Failed to connect to socket for incoming connections.");
-    }
-    setsockopt(this->acceptSocket, SOL_SOCKET, SO_REUSEADDR, (void*)&enable,
-            sizeof(enable));
-
-    //if (!inet_aton(ipaddress.data(), &acceptAddress.sin_addr))
-    //{
-        //error_return(this, "Invalid IP-address");
-    //}
-
-    acceptAddress.sin_port = htons(port);
-    acceptAddress.sin_family = AF_INET;
-    acceptAddress.sin_addr.s_addr = INADDR_ANY;
-    if (0 != bind(this->acceptSocket, (struct sockaddr*)(&acceptAddress),
-            addressSize)) {
-        //error_return(this, "Failed to bind socket to ", ipaddress);
-        error_return(this, "Failed to bind socket to 0.0.0.0:", port);
+        error_return(this, "Failed to determine address to bind socket to: ",
+                gai_strerror(status));
     }
 
-    debug_print(this, "Created socket and bound it to ", inet_ntoa(acceptAddress.sin_addr), ":", port);
-}
-
-/**
- *Listen at the socket, accept incoming connections and delegate them to a worker thread.
- */
-void InConnection::listen_for_clients()
-{
-    int clientSocket = 0;
-    //pthread_t clientThread;
-    //pthread_attr_t clientThreadOptions;
-
-    listen(this->acceptSocket, Config::singleton().get_Tmax());
-    debug_print(this, "Listening for incoming messages");
-
-    //pthread_attr_init(&clientThreadOptions);
-    //pthread_attr_setdetachstate(&clientThreadOptions,
-            //PTHREAD_CREATE_DETACHED);
-
-    while (1) {
-        //pthread_mutex_lock(&clientSocketGuard);
-        clientSocket = accept(this->acceptSocket, NULL, NULL);
-        if (0 > clientSocket) {
-            //pthread_mutex_unlock(&clientSocketGuard);
+    for (auto* it{result}; it != nullptr; it = it->ai_next)
+    {
+        try
+        {
+            resources = std::make_unique<SocketResource> (it->ai_family, it->ai_socktype,
+                        it->ai_protocol);
+        }
+        catch (const BBServException& error)
+        {
+            std::cout << error.what() << std::endl;
             continue;
         }
 
-        //if (0 != pthread_create(&clientThread, &clientThreadOptions,
-                //thread_main, &clientSocket)) {
-            //pthread_mutex_unlock(&clientSocketGuard);
-            //success = EXIT_FAILURE;
-            //break;
-        //}
+        if (this->is_nonblocking())
+        {
+            fcntl(resources->get_accept_socket(), F_SETFL, O_NONBLOCK);
+        }
+
+        if (0 != bind(resources->get_accept_socket(), it->ai_addr,
+                    it->ai_addrlen))
+        {
+            debug_print(this, "Failed to bind socket to ",
+                    inet_ntoa(((sockaddr_in*)result->ai_addr)->sin_addr), ":",
+                    port, " - ", strerror(errno));
+            continue;
+        }
+
+        // All went well with this it's address, so let's take it
+        debug_print(this, "Created socket ", resources->get_accept_socket(),
+                " and bound it to ",
+                inet_ntoa(((sockaddr_in*)result->ai_addr)->sin_addr), ":",
+                port);
+        break;
+    }
+
+    freeaddrinfo(result);
+}
+
+/**
+ * Listen at the socket, accept incoming connections and delegate them to a worker thread.
+ *
+ * In case this instance is operating on a socket in non-blocking mode, it
+ * might throw BBServTimeout.
+ */
+void InConnection::listen_for_clients()
+{
+    auto clientSocket {0};
+
+    listen(resources->get_accept_socket(), Config::singleton().get_Tmax());
+    debug_print(this, "Listening for incoming messages");
+
+    while (1)
+    {
+        // Wait a limited ammount of time for incoming connections if the
+        // socket is in non-blocking mode.
+        if (this->is_nonblocking())
+        {
+            pollfd descriptor;
+            descriptor.fd = resources->get_accept_socket();
+            descriptor.events = POLLIN;
+
+            debug_print(this, "Waiting for data to arrive on socket ",
+                    resources->get_accept_socket());
+
+            auto ready { poll(&descriptor, 1,
+                    Config::singleton().get_network_timeout_ms()) };
+
+            if (0 == ready)
+            {
+                // timeout
+                //timeout_return(this, "Timeout at waiting for incoming network connection");
+
+                //just continue to keep on listening for data replication
+                continue;
+            }
+            else if (-1 == ready)
+            {
+                // error
+                error_return(this, "Failed to poll socket ", resources->get_accept_socket());
+            }
+        }
+
+        debug_print(this, "Accepting connection on socket ", resources->get_accept_socket());
+        clientSocket = accept(resources->get_accept_socket(), NULL, NULL);
+        if (-1 == clientSocket)
+        {
+            debug_print(this, "Failed to accept connection on socket ",
+                    resources->get_accept_socket(), ": ", strerror(errno));
+            break;
+        }
 
         debug_print(this, "Accepted client connection on ", clientSocket);
         this->connectionQueue->add(clientSocket);
+
+        //// Don't keep on waiting for other connections in non-blocking mode.
+        //if (this->is_nonblocking())
+        //{
+            //break;
+        //}
     }
 
-    close(acceptSocket);
-    //pthread_attr_destroy(&clientThreadOptions);
+    debug_print(this, "Stop listening on socket ", resources->get_accept_socket());
+    //close(this->acceptSocket);
+    //this->acceptSocket = 0;
+}
+
+bool InConnection::is_nonblocking()
+{
+    return this->isNonblocking;
 }
 
--- src/InConnection.h
+++ src/InConnection.h
@@ -11,24 +11,101 @@
 
 #pragma once
 
+/**
+ *RAII wrapper for the socket.
+ */
+class SocketResource
+{
+    private:
+        int acceptSocket {0};
+
+    public:
+        SocketResource(int domain, int type, int protocol)
+        {
+            this->acceptSocket = socket(domain, type, protocol);
+            //debug_print(this, "!!! Set socket ", this->acceptSocket, " @", this);
+
+            if (-1 == this->acceptSocket)
+            {
+                error_return(this, "Failed to connect to socket for incoming connections: ",
+                        strerror(errno));
+            }
+
+            auto enable {1};
+            setsockopt(this->get_accept_socket(), SOL_SOCKET, SO_REUSEADDR,
+                    (void*)&enable, sizeof(enable));
+        }
+
+        SocketResource()
+            : acceptSocket(0)
+        { }
+
+        ~SocketResource()
+        {
+            if (0 < this->acceptSocket)
+            {
+                debug_print(this, "Close connection at socket ",
+                        this->acceptSocket, " @", this);
+                close(this->acceptSocket);
+                this->acceptSocket = 0;
+            }
+        }
+
+        SocketResource(const SocketResource& other)
+            : acceptSocket(other.acceptSocket)
+        { }
+
+        SocketResource(SocketResource&& other) noexcept
+            : acceptSocket(std::exchange(other.acceptSocket, 0))
+            { }
+
+        SocketResource& operator=(const SocketResource& other)
+        {
+            return *this = SocketResource(other);
+        }
+
+        SocketResource& operator=(SocketResource&& other) noexcept
+        {
+            std::swap(this->acceptSocket, other.acceptSocket);
+            return *this;
+        }
+
+    public:
+        int get_accept_socket()
+        {
+            return this->acceptSocket;
+        }
+
+};
+
+/**
+ *Network connection listener.
+ */
 class InConnection
 {
     protected:
-        int acceptSocket {0};
+        std::unique_ptr<SocketResource> resources;
         std::shared_ptr<ConnectionQueue> connectionQueue;
-
-    public:
-        InConnection(std::shared_ptr<ConnectionQueue>& qu);
-        ~InConnection();
+        bool isNonblocking {false};
 
     public:
         /**
          * Creates and listens on a socket bound to the given port.
+         *
          * Upon error might throw BBServException.
          */
         void listen_on(/*const std::string_view& ipaddress,*/ in_port_t port);
 
+        /**
+         *Start working in your own thread context.
+         *
+         * Upon error might throw BBServException.
+         */
+        void operate(/*const std::string_view& ipaddress,*/ in_port_t port,
+                std::shared_ptr<ConnectionQueue>& qu, bool isNonblocking = false);
+
     protected:
         void open_incoming_conn(/*const std::string_view& ipaddress,*/ in_port_t port);
         void listen_for_clients();
+        bool is_nonblocking();
 };
